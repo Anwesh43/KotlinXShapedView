88a5a7ceb9288e1e0ff67addc2455a718bb1424b
Anwesh43 <anweshthecool0@gmail.com>
2018-01-11 01:20:49 +0530

    Using XShapedView in activity

--- app/src/main/java/ui/anwesome/com/kotlinxshapedview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinxshapedview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinxshapedview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.xshapedview.XShapedView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        XShapedView.create(this)
     }
 }
