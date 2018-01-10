20d693eee8a45a119e374e48d86c8e057d0f6c28
Anwesh43 <anweshthecool0@gmail.com>
2018-01-11 01:29:40 +0530

    adding OnXShapeListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlinxshapedview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinxshapedview/MainActivity.kt
@@ -2,12 +2,13 @@ package ui.anwesome.com.kotlinxshapedview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.xshapedview.XShapedView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        XShapedView.create(this)
+        val view = XShapedView.create(this)
+        view.addOnXShapeListener({Toast.makeText(this,"Opened",Toast.LENGTH_SHORT).show()},{Toast.makeText(this,"Closed",Toast.LENGTH_SHORT).show()})
     }
 }
