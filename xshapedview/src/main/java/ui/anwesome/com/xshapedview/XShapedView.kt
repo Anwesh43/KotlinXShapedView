d9a982ba157e12672c991df1bef031c590d089f9
Anwesh43 <anweshthecool0@gmail.com>
2018-01-11 00:51:36 +0530

    adding XShaped

--- xshapedview/src/main/java/ui/anwesome/com/xshapedview/XShapedView.kt
+++ xshapedview/src/main/java/ui/anwesome/com/xshapedview/XShapedView.kt
@@ -19,4 +19,32 @@ class XShapedView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class XShaped(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.strokeWidth = size/30
+            paint.color = Color.parseColor("#AD1457")
+            canvas.save()
+            canvas.translate(x,y)
+            for(i in 0..3) {
+                canvas.save()
+                canvas.rotate(i*90f+45f)
+                for(j in 0..1) {
+                    canvas.save()
+                    canvas.translate(0f,-size+j*size/2)
+                    canvas.rotate(90f)
+                    canvas.drawLine(0f,0f,size/2,0f,paint)
+                    canvas.restore()
+                }
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+        
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
