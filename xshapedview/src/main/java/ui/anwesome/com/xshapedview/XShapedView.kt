d15e5f671949ca277d0b40870e6df5af702e1e5e
Anwesh43 <anweshthecool0@gmail.com>
2018-01-11 01:09:47 +0530

    adding Rendererer in View

--- xshapedview/src/main/java/ui/anwesome/com/xshapedview/XShapedView.kt
+++ xshapedview/src/main/java/ui/anwesome/com/xshapedview/XShapedView.kt
@@ -8,13 +8,14 @@ import android.content.*
 import android.graphics.*
 class XShapedView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = Renderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
