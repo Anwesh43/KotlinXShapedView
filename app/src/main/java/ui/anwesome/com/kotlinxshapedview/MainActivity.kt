a4cb14b7cf0015447851799be5a63468afafadfd
Anwesh43 <anweshthecool0@gmail.com>
2018-01-11 08:07:40 +0530

    adding fullscreen method in Activity

--- app/src/main/java/ui/anwesome/com/kotlinxshapedview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinxshapedview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinxshapedview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Toast
 import ui.anwesome.com.xshapedview.XShapedView
 
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val view = XShapedView.create(this)
         view.addOnXShapeListener({Toast.makeText(this,"Opened",Toast.LENGTH_SHORT).show()},{Toast.makeText(this,"Closed",Toast.LENGTH_SHORT).show()})
+        fullScreen()
     }
+}
+fun AppCompatActivity.fullScreen() {
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    supportActionBar?.hide()
 }
